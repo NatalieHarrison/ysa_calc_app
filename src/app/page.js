4c2f0dd1f172d597250c06b05e8fb826b233d643
Natalie Harrison <harrisonNatalie04@outlook.com>
2023-10-31 22:46:11 -0400

    Adding another row of operators

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+"use client"
+
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Box from '@mui/material/Box';
@@ -5,19 +7,20 @@ import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
+import React, { useState } from "react";
+
 export default function Home() {
+  const [input, setInput] = useState("");
   return (
     <Box >
-      <Stack   direction="column" justifyContent="center" alignItems="center" spacing={2}>
+      <Stack  direction="column" justifyContent="center" alignItems="center" spacing={2}>
         <Typography variant='h3'>
-          Natalie's Calculator App
+          Natalie's Calculator App 
         </Typography>
 
-        <Box sx = {{backgroundColor: '#607EAA'}}>
+        <Box sx = {{backgroundColor: 'white'}}>
           <Stack direction = 'column'>
-          <TextField>
-
-          </TextField>
+          <TextField value={input} onChange={(e) => setInput(e.target.value)}/>
           
           <ButtonGroup fullWidth variant="contained" aria-label="outlined primary button group">
             <Button>7</Button>
@@ -25,23 +28,30 @@ export default function Home() {
             <Button>9</Button>
           </ButtonGroup>
 
-          <ButtonGroup fullWidth variant="contained" aria-label="outlined primary button group">
+          <ButtonGroup fullWidth variant="contained" aria-label="outlined primary button group" >
             <Button>4</Button>
             <Button>5</Button>
             <Button>6</Button>
           </ButtonGroup>
           
-          <ButtonGroup fullWidth variant="contained" aria-label="outlined primary button group">
+          <ButtonGroup fullWidth variant="contained" aria-label="outlined primary button group" >
             <Button>1</Button>
             <Button>2</Button>
             <Button>3</Button>
           </ButtonGroup>
-          dsf
-
-          <ButtonGroup fullWidth variant="contained" aria-label="outlined primary button group">
+   
+          <ButtonGroup fullWidth variant="contained" aria-label="outlined primary button group" >
             <Button>0</Button>
             <Button>.</Button>
             <Button>=</Button>
+          </ButtonGroup>
+
+          <ButtonGroup fullWidth variant="contained" aria-label="outlined primary button group">
+            <Button> / </Button>
+            <Button> x </Button>
+            <Button> - </Button>
+            <Button> + </Button>
+
           </ButtonGroup>
           </Stack>
         </Box>
