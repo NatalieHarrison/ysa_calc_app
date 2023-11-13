dc5099ebd40e5522b30a0050eab65ccd48870b92
Natalie Harrison <harrisonNatalie04@outlook.com>
2023-11-13 09:18:24 -0500

    starting point

--- src/app/page.js
+++ src/app/page.js
@@ -12,47 +12,50 @@ import React, { useState } from "react";
 export default function Home() {
   const [input, setInput] = useState("");
   return (
-    <Box >
-      <Stack  direction="column" justifyContent="center" alignItems="center" spacing={2}>
-        <Typography variant='h3'>
+    <Box>
+      <Stack  direction="column" justifyContent="cenetr" alignItems="center" spacing={2}>
+        <Typography variant='h5'>
           Natalie's Calculator App 
         </Typography>
 
-        <Box sx = {{backgroundColor: 'white'}}>
+        <Box sx =  {{backgroundColor: 'white'}}>
           <Stack direction = 'column'>
           <TextField value={input} onChange={(e) => setInput(e.target.value)}/>
           
           <ButtonGroup fullWidth variant="contained" aria-label="outlined primary button group">
-            <Button>7</Button>
-            <Button>8</Button>
-            <Button>9</Button>
+            <Button onClick = {() => setInput(input + "7")}> 7 </Button>
+            <Button onClick = {() => setInput(input + "8")} >8</Button>
+            <Button onClick = {() => setInput(input + "9")} >9</Button>
           </ButtonGroup>
 
           <ButtonGroup fullWidth variant="contained" aria-label="outlined primary button group" >
-            <Button>4</Button>
-            <Button>5</Button>
-            <Button>6</Button>
+            <Button onClick = {() => setInput(input + "4")}> 4 </Button>
+            <Button onClick = {() => setInput(input + "5")}> 5 </Button> 
+            <Button onClick = {() => setInput(input + "6")}> 6</Button>
           </ButtonGroup>
           
           <ButtonGroup fullWidth variant="contained" aria-label="outlined primary button group" >
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
+            <Button onClick = {() => setInput(input + "1")}>1</Button>
+            <Button onClick = {() => setInput(input + "2")}>2</Button>
+            <Button onClick = {() => setInput(input + "3")}>3</Button>
           </ButtonGroup>
    
           <ButtonGroup fullWidth variant="contained" aria-label="outlined primary button group" >
-            <Button>0</Button>
-            <Button>.</Button>
-            <Button>=</Button>
+            <Button onClick = {() => setInput(input + "0")}>0</Button>
+            <Button onClick = {() => setInput(input + ".")}>.</Button>
+            <Button onClick = {() => setInput(input + "=")}>=</Button>
           </ButtonGroup>
 
           <ButtonGroup fullWidth variant="contained" aria-label="outlined primary button group">
-            <Button> / </Button>
-            <Button> x </Button>
-            <Button> - </Button>
-            <Button> + </Button>
+            <Button onClick = {() => setInput(input + "/")}> / </Button>
+            <Button onClick = {() => setInput(input + "x")}> x </Button>
+            <Button onClick = {() => setInput(input + "-")}> - </Button>
+            <Button onClick = {() => setInput(input + "+")}> + </Button>
 
           </ButtonGroup>
+          <Button> Solve </Button>
+
+   
           </Stack>
         </Box>
 
